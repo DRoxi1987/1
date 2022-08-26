06262d6cec83637a38284d1d5afbbc701d45a7dc
DRoxi1987 <dkarr@yandex.ru>
2022-08-27 02:29:16 +0300

    Доработка описания функций. Поменял названия знаков на русские

--- main.py
+++ main.py
@@ -7,35 +7,35 @@ def print_signs(signs: str) -> None:
     print(f"Ваш знак Зодиака: {signs}.\n")
 
 
-def sign_finding(birthday_datetime):
+def sign_finding(birthday_datetime: datetime) -> None:
     """ Сравнивает объект Datetime с интервалами времени, соответствующими
-    определенному знаку в цикле for."""
+    определенному знаку в цикле for. При найденном интервале, вызывает функцию print_signs."""
 
-    dict_sign = [["capricorn", datetime.date(birthday_datetime.year, 12, 22),
+    dict_sign = [["Козерог", datetime.date(birthday_datetime.year, 12, 22),
                   datetime.date(birthday_datetime.year, 12, 31)],
-                 ["capricorn", datetime.date(birthday_datetime.year, 1, 1),
+                 ["Козерог", datetime.date(birthday_datetime.year, 1, 1),
                   datetime.date(birthday_datetime.year, 1, 19)],
-                 ["aquarius", datetime.date(birthday_datetime.year, 1, 20),
+                 ["Водолей", datetime.date(birthday_datetime.year, 1, 20),
                   datetime.date(birthday_datetime.year, 2, 18)],
-                 ["pisces", datetime.date(birthday_datetime.year, 2, 19),
+                 ["Рыбы", datetime.date(birthday_datetime.year, 2, 19),
                   datetime.date(birthday_datetime.year, 3, 20)],
-                 ["aries", datetime.date(birthday_datetime.year, 3, 21),
+                 ["Овен", datetime.date(birthday_datetime.year, 3, 21),
                   datetime.date(birthday_datetime.year, 4, 19)],
-                 ["taurus", datetime.date(birthday_datetime.year, 4, 20),
+                 ["Телец", datetime.date(birthday_datetime.year, 4, 20),
                   datetime.date(birthday_datetime.year, 5, 20)],
-                 ['twins', datetime.date(birthday_datetime.year, 5, 21),
+                 ['Близнецы', datetime.date(birthday_datetime.year, 5, 21),
                   datetime.date(birthday_datetime.year, 6, 20)],
-                 ["crayfish", datetime.date(birthday_datetime.year, 6, 21),
+                 ["Рак", datetime.date(birthday_datetime.year, 6, 21),
                   datetime.date(birthday_datetime.year, 7, 22)],
-                 ["lion", datetime.date(birthday_datetime.year, 7, 23),
+                 ["Лев", datetime.date(birthday_datetime.year, 7, 23),
                   datetime.date(birthday_datetime.year, 8, 22)],
-                 ["virgo", datetime.date(birthday_datetime.year, 8, 23),
+                 ["Дева", datetime.date(birthday_datetime.year, 8, 23),
                   datetime.date(birthday_datetime.year, 9, 22)],
-                 ["scales", datetime.date(birthday_datetime.year, 9, 23),
+                 ["Весы", datetime.date(birthday_datetime.year, 9, 23),
                   datetime.date(birthday_datetime.year, 10, 22)],
-                 ["scorpion", datetime.date(birthday_datetime.year, 10, 23),
+                 ["Скорпион", datetime.date(birthday_datetime.year, 10, 23),
                   datetime.date(birthday_datetime.year, 11, 30)],
-                 ["sagittarius", datetime.date(birthday_datetime.year, 11, 22),
+                 ["Стрелец", datetime.date(birthday_datetime.year, 11, 22),
                   datetime.date(birthday_datetime.year, 12, 21)]]
 
     for i in range(len(dict_sign)):
@@ -44,10 +44,19 @@ def sign_finding(birthday_datetime):
 
 
 def main() -> None:
-    """ Получаем строку от пользователя в формате "день/месяц/год".
-     Проверяем ошибки ввода пользователя. Проверяем является ли год високосным.
-     После проверок переводим строку в объект Datetime и передаем функции
-      sign_finding. """
+    """ Функция запрашивает от пользователя дату рождения в формате "день/месяц/год".
+    Проверяет ошибки ввода пользователя. Проверяет, является ли год високосным.
+
+    Если проверка не прошла, выдает строку: "Введите верную дату" и заново запрашивает дату рождения.
+
+    После проверок переводит строку в объект Datetime и передает функции sign_finding,
+    которая ищет знак Зодиака по полученному объекту. Далее мы получаем на выходе строку:
+    "Ваш знак зодиака: 'Найденное соответствие'.".
+
+    После этого, программа спрашивает: "Выйти из программы y/n?".
+
+    По нажатию клавиши n, опять запрашивает дату рождения от пользователя, либо, по нажатию y,
+    завершает свою работу"""
 
     print("Введите дату рождения: день/месяц/год\n")
     birthday = input()
@@ -63,7 +72,7 @@ def main() -> None:
         print("Введите верную дату")
         main()
 
-    """ Реализация выхода из программы по нажатию y или n. """
+    """ Реализация выхода из программы по нажатию "y" или продолжения использования по нажатию "n". """
 
     print("Выйти из программы y/n?\n")
     exit_function = input()
